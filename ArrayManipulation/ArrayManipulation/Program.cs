64987a6725377b31d0b13adf60e63e87ceb1b2ad
Taylor Smith <taylorsmith4546@gmail.com>
2016-05-03 08:10:09 -0700

    initial commit

---
+++ ArrayManipulation/ArrayManipulation/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayManipulation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] counts = new int[10];
+            counts[0] = 0;
+            counts[1] = 0;
+            counts[2] = 0;
+
+            int[] bonus = new int[4];
+
+        }
+    }
+}
